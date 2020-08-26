d4955287363543d31eae4df60b82dc509d1afab9
Ivan Bodnar <ibodn@softserveinc.com>
2020-08-26 20:27:46 +0300

    removed install command for xlsxwriter

--- run_test/run_test.sh
+++ run_test/run_test.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-python3 -m pip install xlsxwriter
-python3 ../main.py -repos "Oleh-Dmytrash/Lv-490" "graphql/express-graphql" -api_token "./token_file" -pr_n "100"
+python3 ../main.py -repos "Oleh-Dmytrash/Lv-490" "slicknode/graphql-query-complexity" "graphql/express-graphql" -api_token "./token_file" -pr_n "100"
