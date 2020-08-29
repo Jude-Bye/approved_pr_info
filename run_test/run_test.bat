e0a5755f92b4bd407cf7735079b913efe73a6507
JudeByeVZ <vanjazed@gmail.com>
2020-08-30 01:19:34 +0300

    Modified readme and run_test.bat again

--- run_test/run_test.bat
+++ run_test/run_test.bat
@@ -2,5 +2,5 @@ py.exe ../src/main.py^
  -repos "Oleh-Dmytrash/Lv-490" "slicknode/graphql-query-complexity" "graphql/express-graphql"^
  -api_token "./token_file"^
  -pr_n "100"^
- -file_mode split_auto^
+ -file_mode "single" "test_cmd.xlsx"^
  -api_endpoint "https://api.github.com/graphql"
